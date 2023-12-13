0efd6954ed111a16f85f3a62355ad999ffdfd7cd
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-12-13 19:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+let input1 = prompt();
+let input2 = prompt();
+
+
+let result=(parseInt(input1)*2)+parseInt(input2) ;
+alert(`The final result is: ${result}`)
+
+
+
+
+
+
